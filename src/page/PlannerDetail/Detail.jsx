73c865ea98a38d42e83f42ea5968f3919d13043d
Dayoon Kim <danakim21@gmail.com>
2021-07-11 01:23:12 +0900

    [#12] 플래너 담당 코드 수정

--- src/page/PlannerDetail/Detail.jsx
+++ src/page/PlannerDetail/Detail.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import BoldTitle from '../../component/BoldTitle';
-import styled from 'styled-components';
+import * as $ from './DetailView';
 
 const Detail = () => {
   const DETAIL_LIST = ['서울', '경기', '동행하는 플래너에요', '모바일청첩장을 제공해요', '제휴웨딩홀이 있어요'];
 
   return (
     <div>
-      <NewTitle>플래너가 담당해요</NewTitle>
+      <$.NewTitle>플래너가 담당해요</$.NewTitle>
       {DETAIL_LIST.map((detail) => (
-        <DetailItem>체크! {detail}</DetailItem>
+        <$.DetailItem>체크! {detail}</$.DetailItem>
       ))}
     </div>
   );
 };
 
 export default Detail;
-
-const NewTitle = styled(BoldTitle)`
-  margin-bottom: 13px;
-`;
-
-const DetailItem = styled.span`
-  margin-right: 8px;
-`;
---
+++ src/page/PlannerDetail/DetailView.js
@@ -0,0 +1,10 @@
+import BoldTitle from '../../component/BoldTitle';
+import styled from 'styled-components';
+
+export const NewTitle = styled(BoldTitle)`
+  margin-bottom: 13px;
+`;
+
+export const DetailItem = styled.span`
+  margin-right: 8px;
+`;

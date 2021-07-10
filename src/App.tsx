da6fef14442652bccee6bf63709d72628d3b8024
minkukjo <strike0115@naver.com>
2021-07-11 04:07:38 +0900

    [#6] 메인 페이지 마크업

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Main from './page/Home/Main/Main';
 import Login from './page/User/Login/Login';
 import PrivateRoute from './routes/PrivateRoute';
 
@@ -6,7 +7,9 @@ const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/"></Route>
+        <Route exact path="/">
+          <Main />
+        </Route>
         <Route path="/login">
           <Login />
         </Route>

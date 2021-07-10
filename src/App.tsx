2bbca845c81513278b7404e5e41f9809ccafb01c
angrycatjenny <angrycatjenny@gmail.com>
2021-07-10 19:55:00 +0900

    [#5] 로그인화면 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './page/Login';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/"></Route>
+        <Route exact path="/"></Route>
+        <Route path="/login">
+          <Login />
+        </Route>
       </Switch>
     </BrowserRouter>
   );

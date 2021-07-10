17e74b6e3d68a074a4fd093276f601e4e57d9946
angrycatjenny <angrycatjenny@gmail.com>
2021-07-11 04:22:08 +0900

    [#14] 일반회원 나의페이지 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './page/User/Login/Login';
 import PrivateRoute from './routes/PrivateRoute';
+import UserPage from './page/User/MyPage/UserPage';
+import '../src/styles/reset.css';
 
 const App = () => {
   return (
@@ -9,6 +11,9 @@ const App = () => {
         <Route exact path="/"></Route>
         <Route path="/login">
           <Login />
+        </Route>
+        <Route path="/userPage">
+          <UserPage />
         </Route>
       </Switch>
     </BrowserRouter>

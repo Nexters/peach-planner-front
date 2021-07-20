2542885019c82537b89094af8f7d87f3474477e8
kiram <angrycatjenny@gmail.com>
2021-07-20 23:44:51 +0900

    HashRouter 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from './page/Home/Main/Main';
 import PlannerDetail from './page/PlannerDetail/PlannerDetail';
 import Login from './page/User/Login/Login';
@@ -13,30 +13,32 @@ import Search from './page/Home/Search/Search';
 const App = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Main />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
-        <Route path="/detail">
-          <PlannerDetail />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/estimate">
-          <PlannerEstimate />
-        </Route>
-        <Route path="/userPage">
-          <UserPage />
-        </Route>
-        <Route path="/plannerPage">
-          <PlannerPage />
-        </Route>
-      </Switch>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/detail">
+            <PlannerDetail />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/estimate">
+            <PlannerEstimate />
+          </Route>
+          <Route path="/userPage">
+            <UserPage />
+          </Route>
+          <Route path="/plannerPage">
+            <PlannerPage />
+          </Route>
+        </Switch>
+      </Router>
     </BrowserRouter>
   );
 };

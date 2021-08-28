6354f320f6659a9e9af8dfc23ec8e1b3bcc39984
minkukjo <strike0115@naver.com>
2021-08-28 16:11:17 +0900

    [#42] 로그인 경로 이슈 핫픽스

---
+++ src/api/Login.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export interface UserRequest {
+  name?: string;
+  nickName?: string;
+  userName: string;
+  password: string;
+  type?: 'USER';
+  loginType?: 'BASIC';
+}
+
+export const fetchToken = async (request: UserRequest) => {
+  const { data } = await axios.post<any>(`/auth/login`, request);
+  return data;
+};
+
+export const fetchSignUp = async (request: UserRequest) => {
+  const { data } = await axios.post<any>(`/auth/signup`, request);
+  return data;
+};
+
+export const fetchRefreshToken = async (refreshToken: any, config: any) => {
+  const { data } = await axios.post<any>(`/auth/token/refresh`, { refreshToken }, config);
+  return data;
+};

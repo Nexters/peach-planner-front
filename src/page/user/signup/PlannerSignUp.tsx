d8aa82eb898ea8d55ae982bfa933b26f8bed4834
kiram <grim@tanagement.net>
2021-09-08 21:54:13 +0900

    플래너 가입 css 해결 및 가입 로직 적용

--- src/page/user/signup/PlannerSignUp.tsx
+++ src/page/user/signup/PlannerSignUp.tsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import PButton from 'src/component/PButton';
 import HorizontalLine from 'src/component/HorizontalLine';
 import { FlexDiv, Content, Title } from 'src/component/style/style';
+import { useHistory } from 'react-router-dom';
+import { User } from 'src/api/Planner';
+import axios from 'axios';
 
 const emailRegExp = /^[0-9a-z]([-_\.]?[0-9a-z])*@[0-9a-z]([-_\.]?[0-9a-z])*\.[a-z]/;
 const passwordRegExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+const phoneRegExp = /^\d{3}\d{3,4}\d{4}$/;
 
 const PlannerSignUp = () => {
+  const history = useHistory();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
@@ -15,6 +21,29 @@ const PlannerSignUp = () => {
   const [isValidPassword, setIsValidPassword] = useState(true);
   const [name, setName] = useState('');
   const [checkAll, setCheckAll] = useState(false);
+  const [phone, setPhone] = useState('');
+  const [agreeTerms, setAgreeTerms] = useState(false);
+  const [agreePrivacy, setAgreePrivacy] = useState(false);
+
+  useEffect(() => {
+    if (checkAll) {
+      setAgreeTerms(true);
+      setAgreePrivacy(true);
+    } else {
+      if (agreeTerms && agreePrivacy) {
+        setAgreeTerms(false);
+        setAgreePrivacy(false);
+      }
+    }
+  }, [checkAll]);
+
+  useEffect(() => {
+    if (!agreeTerms || !agreePrivacy) {
+      setCheckAll(false);
+    } else {
+      setCheckAll(true);
+    }
+  }, [agreeTerms, agreePrivacy]);
 
   const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -33,12 +62,101 @@ const PlannerSignUp = () => {
   };
 
   const handleCheckAll = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(checkAll);
     setCheckAll(!checkAll);
   };
 
+  const handleCheckTerms = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAgreeTerms(!agreeTerms);
+  };
+
+  const handleCheckPrivacy = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAgreePrivacy(!agreePrivacy);
+  };
+
+  const handlePhone = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhone(e.target.value);
+  };
+
+  const handleSignUp = () => {
+    if (!email) {
+      alert('이메일을 입력해주세요.');
+    } else if (email.match(emailRegExp) === null) {
+      setIsValidEmail(false);
+    } else if (!password) {
+      alert('비밀번호를 입력해주세요.');
+      setIsValidEmail(true);
+    } else if (!passwordConfirm) {
+      alert('비밀번호를 한번 더 입력해주세요.');
+      setIsValidEmail(true);
+    } else if (password !== passwordConfirm) {
+      setIsValidPassword(false);
+      setIsValidEmail(true);
+    } else if (!name) {
+      alert('이름을 입력해주세요.');
+      setIsValidPassword(true);
+      setIsValidEmail(true);
+    } else if (!phone) {
+      alert('핸드폰 번호를 입력해주세요.');
+      setIsValidPassword(true);
+      setIsValidEmail(true);
+    } else if (phone.match(phoneRegExp) === null) {
+      alert('핸드폰 번호를 올바른 형식으로 입력해주세요.');
+      setIsValidPassword(true);
+      setIsValidEmail(true);
+    } else if (!agreeTerms) {
+      alert('이용약관에 동의해주세요.');
+      setIsValidPassword(true);
+      setIsValidEmail(true);
+    } else if (!agreePrivacy) {
+      alert('개인정보 처리방침에 동의해주세요.');
+      setIsValidPassword(true);
+      setIsValidEmail(true);
+    } else {
+      signup({
+        name,
+        userName: email,
+        password,
+        type: 'PLANNER'
+      });
+    }
+  };
+
+  const signup = async (data: User) => {
+    const res = await axios.post('/auth/signup', data);
+    login({
+      userName: email,
+      password,
+      loginType: 'BASIC'
+    });
+  };
+
+  const login = async (data: User) => {
+    const res = await axios.post('/auth/login', data);
+    localStorage.setItem('accessToken', res.data.accessToken);
+    localStorage.setItem('refreshToken', res.data.refreshToken);
+    history.push('/');
+    window.location.reload();
+    alert('회원가입 및 로그인이 완료되었습니다.');
+    // axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.accessToken}`;
+    // const expireTime = Date.parse(res.data.expireDateTime);
+    // setTimeout(refreshToken, expireTime - 60000);
+  };
+
+  const refreshToken = async () => {
+    const refreshToken = localStorage.getItem('refreshToken');
+    const access = localStorage.getItem('accessToken');
+    const config = {
+      headers: {
+        Authorization: `Bearer ${access}`
+      }
+    };
+    const res = await axios.post('/auth/token/refresh', { refreshToken }, config);
+    localStorage.setItem('accessToken', res.data.accessToken);
+    localStorage.setItem('refreshToken', res.data.refreshToken);
+  };
+
   return (
-    <>
+    <FlexDiv direction="column">
       <Title color={'#212529'} height={'24px'} fontSize={'18px'} lineHeight={'27px'} margin={'40px 0 24px'}>
         웨딩플래너 가입하기{' '}
       </Title>
@@ -85,11 +203,11 @@ const PlannerSignUp = () => {
 
         <Label>휴대폰인증</Label>
         <Input
-          value={email}
+          value={phone}
           placeholder="휴대폰 번호를 입력해주세요."
           type="text"
-          id="inputEmail"
-          onChange={handleEmail}
+          id="inputPhone"
+          onChange={handlePhone}
         />
 
         <HorizontalLine top="40px" bottom="40px" />
@@ -101,18 +219,18 @@ const PlannerSignUp = () => {
         <HorizontalLine top="40px" bottom="40px" />
 
         <FlexDiv margin="0">
-          <CheckBox checked={checkAll} id="checkAll" onChange={handleCheckAll} />
+          <CheckBox checked={agreeTerms} id="checkAll" onChange={handleCheckTerms} />
           <Label>[필수] 피치플래너 이용약관 동의</Label>
         </FlexDiv>
         <FlexDiv margin="15px 0 43px 0">
-          <CheckBox checked={checkAll} id="checkAll" onChange={handleCheckAll} />
+          <CheckBox checked={agreePrivacy} id="checkAll" onChange={handleCheckPrivacy} />
           <Label>[필수] 개인정보 처리방침 동의</Label>
         </FlexDiv>
-        <PButton color="pink" width="100%" height="33px" fontSize="12px" padding="0">
+        <PButton color="pink" width="100%" height="33px" fontSize="12px" padding="0" onClick={handleSignUp}>
           가입 완료하기
         </PButton>
       </FlexDiv>
-    </>
+    </FlexDiv>
   );
 };
 
@@ -134,6 +252,9 @@ const Label = styled.label`
   color: #495057;
   line-height: 20px;
   margin-bottom: 5.5px;
+  &:not(:first-child) {
+    margin-top: 4px;
+  }
 `;
 
 const Input = styled.input.attrs((props) => ({

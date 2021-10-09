459496f6620048f6eaf19e0b390ccfeb227b95c6
kiram <angrycatjenny@gmail.com>
2021-10-09 21:18:56 +0900

    마이페이지 이동

--- src/api/User.tsx
+++ src/api/User.tsx
@@ -4,10 +4,11 @@ import { QueryFunctionContext } from 'react-query';
 export interface User {
   id?: number;
   name: string;
+  userType?: string;
 }
 
 export const getUser = async ({ queryKey }: QueryFunctionContext) => {
-      const [_key, params] = queryKey;
+  const [_key, params] = queryKey;
   const { data } = await axios.get<User>(`/users/me`, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem('accessToken')}`

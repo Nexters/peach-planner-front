3863e581957e18332cbf1664fe99d24d44346587
minkukjo <strike0115@naver.com>
2021-11-12 22:43:21 +0900

    토큰 만료 시 Refresh 토큰으로 재발급 or 로그인 이동

--- src/api/dist/index.js
+++ src/api/dist/index.js
@@ -9,4 +9,32 @@ exports.setAxiosDefaults = function () {
         'Content-Type': 'application/json',
         Accept: 'application/json;charset=utf-8'
     };
+    axios_1["default"].interceptors.response.use(function (response) {
+        return response;
+    }, function (error) {
+        var originalRequest = error.config;
+        if (error.response.status === 500 && originalRequest.url === axios_1["default"].defaults.baseURL + 'token/refresh/') {
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('refreshToken');
+            window.location.href = '/login/';
+            return Promise.reject(error);
+        }
+        if (error.response.data.status === 401) {
+            var refreshToken = localStorage.getItem('refresh_token');
+            return axios_1["default"]
+                .post('/auth/token/refresh', { refreshToken: refreshToken })
+                .then(function (response) {
+                localStorage.setItem('accessToken', response.data.accessToken);
+                localStorage.setItem('refreshToken', response.data.refreshToken);
+                return axios_1["default"](originalRequest);
+            })["catch"](function (err) {
+                console.log(err);
+            });
+        }
+        else {
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('refreshToken');
+            window.location.href = '/login/';
+        }
+    });
 };
--- src/api/index.tsx
+++ src/api/index.tsx
@@ -7,4 +7,37 @@ export const setAxiosDefaults = () => {
     'Content-Type': 'application/json',
     Accept: 'application/json;charset=utf-8'
   };
+
+  axios.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      const originalRequest = error.config;
+      if (error.response.status === 500 && originalRequest.url === axios.defaults.baseURL + 'token/refresh/') {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        window.location.href = '/login/';
+        return Promise.reject(error);
+      }
+
+      if (error.response.data.status === 401) {
+        const refreshToken = localStorage.getItem('refresh_token');
+        return axios
+          .post('/auth/token/refresh', { refreshToken: refreshToken })
+          .then((response) => {
+            localStorage.setItem('accessToken', response.data.accessToken);
+            localStorage.setItem('refreshToken', response.data.refreshToken);
+            return axios(originalRequest);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        window.location.href = '/login/';
+      }
+    }
+  );
 };

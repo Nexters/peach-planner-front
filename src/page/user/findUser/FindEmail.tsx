5c6cd8d1de6b1c023343d55e054050b75c68bb82
SunMyeong Lee <actumn814@gmail.com>
2022-01-14 01:01:58 +0900

    Implement reset password page (#187)

--- src/page/user/findUser/FindEmail.tsx
+++ src/page/user/findUser/FindEmail.tsx
@@ -4,12 +4,14 @@ import PButton from 'src/component/PButton';
 import { FlexDiv, Content, Title } from 'src/component/style/style';
 import { useHistory } from 'react-router-dom';
 import { FindUserByEmail } from 'src/api/User';
+import { sendResetEmail } from 'src/api/ResetPw';
 
 const FindEmailPage = () => {
   const history = useHistory();
   const [email, setEmail] = useState('');
   const [check, setCheck] = useState(false);
   const [exist, setExist] = useState(false);
+  const [sent, setSent] = useState(false);
 
   const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -34,57 +36,94 @@ const FindEmailPage = () => {
 
   return (
     <FlexDiv direction="column" height={'700px'} justify={'flex-start'}>
-      <Title color={'#212529'} height={'24px'} fontSize={'18px'} lineHeight={'27px'} margin={'40px 0 15px'}>
-        이메일 / 비밀번호 찾기
-      </Title>
-      <Span>회원가입 시 등록한 이메일 주소를 입력 후 가입여부를 확인해 주세요.</Span>
+      {sent ? (
+        <>
+          <Title color={'#212529'} height={'27px'} fontSize={'18px'} lineHeight={'27px'} margin={'40px 0 15px'}>
+            이메일 발송 완료
+          </Title>
+          <Span>
+            비밀번호 재설정을 위한 Email 이 사용자의 계정으로 전송되었습니다.<br /><br />
 
-      {check ? (
-        <FlexDiv justify="flex-start" align="center" direction="column" width={'312px'}>
-          <Email>{email}</Email>
-          <Span>{exist ? '가입하신 이메일이 존재합니다.' : '가입하신 이메일이 존재하지 않습니다.'}</Span>
-          <PButton
-            color="pink"
-            width="100%"
-            height="40px"
-            fontSize="12px"
-            padding="0"
-            onClick={() => history.push('/login')}
-            margin={'5px 0 10px '}
-          >
-            로그인하기
-          </PButton>
-          {exist && (
+            받으신 이메일을 확인하시고 안내에 따라 비밀번호를 재설정 하세요.<br />
+            만약 메일을 받지 못하셨다면, 스팸 메일함을 확인해 주세요.
+          </Span>
+          <FlexDiv justify="flex-start" align="center" direction="column" width={'398px'}>
+
             <PButton
-              otherBgColor="#f1f3f5"
+                  otherBgColor="#f1f3f5"
               width="100%"
               height="40px"
               fontSize="12px"
               padding="0"
               border="none"
-              onClick={() => history.push('/resetPw')}
+              onClick={async () => {
+                await sendResetEmail(email);
+                alert("메일이 재발송되었습니다. 메일함을 확인해주세요.");
+              }}
+              margin={'5px 0 10px '}
             >
-              비밀번호 재설정하기
-            </PButton>
-          )}
-        </FlexDiv>
-      ) : (
-        <form onSubmit={handleCheck}>
-          <FlexDiv justify="flex-start" align="flex-start" direction="column" width="undefined">
-            <Label>이메일</Label>
-            <Input
-              value={email}
-              placeholder="이메일을 입력해 주세요."
-              type="text"
-              id="inputEmail"
-              onChange={handleEmail}
-            />
-
-            <PButton color="pink" width="100%" height="40px" fontSize="12px" padding="0" margin={'36px 0 0 '}>
-              가입여부 확인
+              인증메일 재발송
             </PButton>
           </FlexDiv>
-        </form>
+        </>
+      ) : (
+        <>
+          <Title color={'#212529'} height={'24px'} fontSize={'18px'} lineHeight={'27px'} margin={'40px 0 15px'}>
+            이메일 / 비밀번호 찾기
+          </Title>
+          <Span>회원가입 시 등록한 이메일 주소를 입력 후 가입여부를 확인해 주세요.</Span>
+
+          {check ? (
+            <FlexDiv justify="flex-start" align="center" direction="column" width={'312px'}>
+              <Email>{email}</Email>
+              <Span>{exist ? '가입하신 이메일이 존재합니다.' : '가입하신 이메일이 존재하지 않습니다.'}</Span>
+              <PButton
+                color="pink"
+                width="100%"
+                height="40px"
+                fontSize="12px"
+                padding="0"
+                onClick={() => history.push('/login')}
+                margin={'5px 0 10px '}
+              >
+                로그인하기
+              </PButton>
+              {exist && (
+                <PButton
+                  otherBgColor="#f1f3f5"
+                  width="100%"
+                  height="40px"
+                  fontSize="12px"
+                  padding="0"
+                  border="none"
+                  onClick={async () => {
+                    await sendResetEmail(email);
+                    setSent(true);
+                  }}
+                >
+                  비밀번호 재설정하기
+                </PButton>
+              )}
+            </FlexDiv>
+          ) : (
+            <form onSubmit={handleCheck}>
+              <FlexDiv justify="flex-start" align="flex-start" direction="column" width="undefined">
+                <Label>이메일</Label>
+                <Input
+                  value={email}
+                  placeholder="이메일을 입력해 주세요."
+                  type="text"
+                  id="inputEmail"
+                  onChange={handleEmail}
+                />
+
+                <PButton color="pink" width="100%" height="40px" fontSize="12px" padding="0" margin={'36px 0 0 '}>
+                  가입여부 확인
+                </PButton>
+              </FlexDiv>
+            </form>
+          )}
+        </>
       )}
     </FlexDiv>
   );
@@ -93,7 +132,7 @@ const FindEmailPage = () => {
 export default FindEmailPage;
 
 const Span = styled.span`
-  height: 21px;
+  width: 498px;
   font-family: SpoqaHanSans;
   font-size: 14px;
   font-weight: normal;
@@ -101,7 +140,7 @@ const Span = styled.span`
   font-style: normal;
   line-height: normal;
   letter-spacing: normal;
-  text-align: left;
+  text-align: center;
   color: #495057;
   margin-bottom: 40px;
 `;

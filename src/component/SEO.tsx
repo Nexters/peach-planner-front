4725ba3c3757988e4f5f039043ed8ecfe2ee4c76
SunMyeong Lee <actumn814@gmail.com>
2022-01-22 02:53:11 +0900

    Add hard-coded meta info

--- src/component/SEO.tsx
+++ src/component/SEO.tsx
@@ -1,21 +1,21 @@
 import { Helmet } from 'react-helmet'
 import defaultImage from '../assets/img/img_background.png';
 
-export default function SEO({ 
-    title = "피치플래너", 
-    ogTitle = "웨딩플래너 비교서비스, 피치플래너",
-    description = "한번뿐인 결혼식, 믿을 수 있는 웨딩플래너를 피치플래너에서 찾아보세요.", 
-    image = defaultImage,
-    siteTitle = "",
-}: {title?: string; ogTitle?: string; description?: string; siteTitle?: string; image?: string;}) {
+export default function SEO({
+  title = "피치플래너",
+  ogTitle = "웨딩플래너 비교서비스, 피치플래너",
+  description = "한번뿐인 결혼식, 믿을 수 있는 웨딩플래너를 피치플래너에서 찾아보세요.",
+  image = defaultImage,
+  siteTitle = "",
+}: { title?: string; ogTitle?: string; description?: string; siteTitle?: string; image?: string; }) {
   return (
     <Helmet
       title={title}
       titleTemplate={siteTitle ? `%s | ${siteTitle}` : undefined}
       meta={[
         {
-            name: `viewport`,
-            content: "width=device-width, initial-scale=1.0",
+          name: `viewport`,
+          content: "width=device-width, initial-scale=1.0",
         },
         {
           name: `description`,
@@ -58,16 +58,16 @@ export default function SEO({
           content: description,
         },
         {
-            property: `robots`,
-            content: "ALL",
+          name: `robots`,
+          content: "ALL",
         },
         {
-            property: `robots`,
-            content: "웨딩플래너,웨딩업체,결혼준비,결혼,웨딩플래너업체",
+          name: `robots`,
+          content: "웨딩플래너,웨딩업체,결혼준비,결혼,웨딩플래너업체",
         },
         {
-            property: `author`,
-            content: "피치플래너",
+          name: `author`,
+          content: "피치플래너",
         },
       ]}
     />

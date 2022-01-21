8c7adce20129a29ec4fbb5e5b3c797f403717746
SunMyeong Lee <actumn814@gmail.com>
2022-01-22 01:36:59 +0900

    Add react-helmet for seo

--- src/page/planner-detail/index.tsx
+++ src/page/planner-detail/index.tsx
@@ -12,6 +12,8 @@ import { Planner } from '../../api/Planner';
 import axios from 'axios';
 import Interaction from './Interaction';
 import { useUserTypeState } from 'src/atoms/AuthStatus';
+import { Helmet } from "react-helmet";
+import backgroundImage from "../../assets/img/img_background.png";
 
 interface routeProps {
   id: string;
@@ -47,6 +49,20 @@ const PlannerDetail = () => {
 
   return plannerInfo ? (
     <Container>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>피치플래너</title>
+        <meta name="description" content="한번뿐인 결혼식, 믿을 수 있는 웨딩플래너를 피치플래너에서 찾아보세요."/>
+        <meta name="robots" content="ALL"/>
+        <meta name="robots" content="웨딩플래너,웨딩업체,결혼준비,결혼,웨딩플래너업체" />
+        <meta name="author" content="피치플래너"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content={`웨딩플래너 - ${plannerInfo.name} 플래너`}/>
+        <meta property="og:description" content={plannerInfo.description || "한번뿐인 결혼식, 믿을 수 있는 웨딩플래너를 피치플래너에서 찾아보세요."}/>
+        <meta property="og:image" content={plannerInfo.images.length > 0 ? plannerInfo.images[0] : backgroundImage}/>
+        <meta property="og:url" content="http://peachplanner.com/"/>
+        <meta charSet="utf-8" />
+      </Helmet>
       <Summary plannerInfo={plannerInfo}>
         {userType === 'USER' ? (
           <Interaction plannerInfo={plannerInfo} setPlannerInfo={setPlannerInfo}></Interaction>

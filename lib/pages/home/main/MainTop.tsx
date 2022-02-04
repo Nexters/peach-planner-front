74d9d4bd9a30bba1ac5c226efc011038947c634e
SunMyeong Lee <actumn814@gmail.com>
2022-02-04 20:55:15 +0900

    Change main banner

--- lib/pages/home/main/MainTop.tsx
+++ lib/pages/home/main/MainTop.tsx
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 import { FlexDiv } from 'lib/pages/components/style/style';
-import background from 'public/assets/img/img_background.png';
+// import background from 'public/assets/img/img_background.png';
+import background from 'public/banner.png';
+import Link from 'next/link';
 
 const MainTop = () => {
   return (
-    <TopBox>
-      <FlexDiv margin={'97px 0 0 73px'} justify="flex-start" align="start" direction="column">
+    <Link href="https://blog.naver.com/peachplanner/222637086412">
+      <a>
+        <TopBox>
+          {/* <FlexDiv margin={'97px 0 0 73px'} justify="flex-start" align="start" direction="column">
         <Title>
           나와 맞는 웨딩플래너, <br></br>피치플래너에서 찾아보세요
         </Title>
@@ -13,8 +17,10 @@ const MainTop = () => {
           한번뿐인 결혼식,믿을 수 있는 웨딩플래너를 찾기 힘드신가요? <br></br>피치플래너에서 검증된 웨딩플래너를
           찾아보세요.
         </Content>
-      </FlexDiv>
-    </TopBox>
+      </FlexDiv> */}
+        </TopBox>
+      </a>
+    </Link>
   );
 };
 

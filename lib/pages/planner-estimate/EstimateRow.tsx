9860ad6da2d67672b441740f7f6c75b2c950dc40
actumn <actumn@users.noreply.github.com>
2022-02-26 13:16:15 +0000

    deploy: 9406e879d92f3e072c06549d45ac62ed048738c8

--- lib/pages/planner-estimate/EstimateRow.tsx
+++ lib/pages/planner-estimate/EstimateRow.tsx
@@ -8,8 +8,8 @@ interface EstimateRowProps {
 
 const EstimateRow: FC<EstimateRowProps> = ({ label, children }) => {
   return (
-    <Row>
-      <Label>{label}</Label>
+    <Row label={label}>
+      <Label label={label}>{label}</Label>
       <Content>{children}</Content>
     </Row>
   );
@@ -17,16 +17,17 @@ const EstimateRow: FC<EstimateRowProps> = ({ label, children }) => {
 
 export default EstimateRow;
 
-const Row = styled.div`
+const Row = styled.div<{label?:string}>`
   display: flex;
   margin-top: 15px;
-  align-items: center;
+  align-items: ${(props) => props.label === '스튜디오' || props.label==='드레스' ||  props.label==='메이크업' ? 'flex-start' : 'center'};
 `;
 
-const Label = styled.label`
+const Label = styled.label<{label?: string}>`
   flex: 1;
   font-size: 16px;
   color: #868e96;
+  margin-top: ${(props) => props.label === '스튜디오' || props.label==='드레스' ||  props.label==='메이크업' ? '15px' : '0'};
 `;
 
 const Content = styled.div`
